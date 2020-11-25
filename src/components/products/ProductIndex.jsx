80ad0468fe82b7940717bfdb37c1bad5131755ee
Taimoor khan <khant8k83@gmail.com>
2020-11-25 18:19:34 +0500

    feat: Added bootstrap => products

--- src/components/products/ProductIndex.jsx
+++ src/components/products/ProductIndex.jsx
@@ -1,20 +1,33 @@
-import { Link  } from "react-router-dom";
-import { ProductData } from './ProductData'
+import { Link } from "react-router-dom";
+import { ProductData } from "./ProductData";
 
-const ProductIndex = () =>{
-    console.log(ProductData)
-    return(
-        <div>
-            <h2>Products Index</h2>
-            {
-            ProductData.map(({id, name, image}) =>
-                <li>
-                    <Link key={id} to={id}>{name}<br/> <img src={image} alt={name}/> </Link>
-                </li>
-                )
-            }
+const ProductIndex = () => {
+  console.log(ProductData);
+  return (
+    <div className="jsutify-content-center ">
+        <div className="text-center ">
+      <h2 className={`pt-2 mt-2`}>PRODUCTS</h2>
+      <p className={`pt-0 mt-0`}>Grab the latest collection of <strong>| Show Case |</strong> Now</p>
+
         </div>
-    )
-}
+      <div className="d-flex flex-wrap justify-content-center">
+      {ProductData.map(({ id, name, image, details }) => (
+        <div key={id} className="p-2">
+          <div className={`card`} style={{width: "18rem"}}>
+            <img class={`card-img-top`} src={image} alt={name} />
+            <div className={`card-body`}>
+              <h5 className={`card-title`}>{name}</h5>
+              <p className={`card-text`}>{details}</p>
+              <Link to={id} className={`btn btn-primary`}>
+                Get It Now
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+};
 
-export default ProductIndex
+export default ProductIndex;
