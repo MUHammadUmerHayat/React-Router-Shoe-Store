6cbe5f71c703b742485dc83502df70c9ca8b114a
Taimoor khan <khant8k83@gmail.com>
2020-11-26 16:48:52 +0500

    feat: added CSS =>product index -> cards

--- src/components/products/ProductIndex.jsx
+++ src/components/products/ProductIndex.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { ProductData } from "./ProductData";
+import style from '../../App.module.css'
 
 const ProductIndex = () => {
   console.log(ProductData);
@@ -11,13 +12,15 @@ const ProductIndex = () => {
 
         </div>
       <div className="d-flex flex-wrap justify-content-center">
-      {ProductData.map(({ id, name, image, details }) => (
+      {ProductData.map(({ id, name, sizes , image, details }) => (
         <div key={id} className="p-2">
           <div className={`card`} style={{width: "18rem"}}>
             <img class={`card-img-top`} src={image} alt={name} />
             <div className={`card-body`}>
-              <h5 className={`card-title`}>{name}</h5>
-              <p className={`card-text`}>{details}</p>
+              <h5 className={`card-title `}>{name}</h5>
+              <div className={style.size_container}>
+              { sizes.map((size) => (<div className={style.size}>{size}</div>)) }
+              </div>
               <Link to={id} className={`btn btn-primary`}>
                 Get It Now
               </Link>
